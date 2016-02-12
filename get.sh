6e4a37ff2e5866c89a3231b76dfbe1da2514ee14
BourgondAries <macocio@gmail.com>
2016-02-12 04:14:50 +0100

    Finish getting the desired images.

--- get.sh
+++ get.sh
@@ -1,10 +1,10 @@
-tags=$(echo "$1" | sed 's/ /%20/g')
+tags=$(echo "$1" | sed 's/ /%20/g' | sed 's/\//%25-2F/g')
 while [ true ]; do
 	wget "https://e621.net/post/index/1/$tags?searchDefault=Search" -O temporary
 	lastpage=$(cat temporary | grep 'searchDefault=Search' | sed "s/<\/a>/\n/g" | tail -n 3 | head -n 1 | cut -d '>' -f 2)
 
 	randompage=$(shuf -i 1-$lastpage -n 1)
-	wget "https://e621.net/post/index/$randompage/mlp%20rating:e?searchDefault=Search" -O temporary
+	wget "https://e621.net/post/index/$randompage/$tags?searchDefault=Search" -O temporary
 	posts=$(cat temporary | grep 'class="thumb"' | sed 's/<span.*href=//g' | cut -d '"' -f 2)
 
 	postcount=$(echo "$posts" | wc -l)
@@ -13,14 +13,23 @@ while [ true ]; do
 	link=$(echo "$posts" | head -n $randompost | tail -n 1)
 	link="https://e621.net$link"
 
-	echo "$link" | xsel --clipboard
-	xdotool mousemove 600 950
-	sleep 1s
-	xdotool click 1
-	sleep 1s
-	xdotool keydown ctrl
-	xdotool key v
-	xdotool keyup ctrl
-	sleep 1s
-	xdotool key KP_Enter
+	wget "$link" -O temporary
+	file=$(cat temporary | grep static | head -n 4 | tail -n 1 | cut -d '"' -f 2)
+	wget "$file" -O tmpimg
+	image_tags=$(cat temporary | grep post_tags | head -n 2 | tail -n 1 | cut -d '>' -f 2 | cut -d '<' -f 1 | sed 's/\//+/g')
+	convert tmpimg tmpimg
+	mv tmpimg "$image_tags".png
+
+	if [ false ]; then
+		echo "$link" | xsel --clipboard
+		xdotool mousemove 600 950
+		sleep 1s
+		xdotool click 1
+		sleep 1s
+		xdotool keydown ctrl
+		xdotool key v
+		xdotool keyup ctrl
+		sleep 1s
+		xdotool key KP_Enter
+	fi
 done
